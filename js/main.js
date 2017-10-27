7be0a5d6dd301b56928d0cc4145aff80337e8b0e
Appliberated.com <develop@appliberated.com>
2017-10-27 13:22:54 +0300

    Simplify, restructure and cleanup main JavaScript
    * remove Blur event listening functionality and url query parameters
    * add source code comments

--- js/main.js
+++ js/main.js
@@ -1,78 +1,75 @@
+/**
+ * leave page
+ * https://leavepage.appliberated.com/
+ *
+ * Copyright (c) 2017 Appliberated
+ * Released under the MIT license
+ * https://github.com/appliberated/LeavePage/blob/master/LICENSE
+ */
+
 "use strict";
 
-const COMEBACK_TITLE = "Come back to me";
-const LEAVE_TITLE = "Leave page";
-
 class App {
-    
 
+    /**
+     * Cache some frequently used DOM elements.
+     */
     constructor() {
-        this.invisibleAudio = document.getElementById("invisibleAudio");
+        this.comeBackAudio = document.getElementById("comeBackAudio");
         this.portrait = document.getElementById("portrait");
     }
 
-    comeBack() {
-        document.title = COMEBACK_TITLE;
-        
-        this.invisibleAudio.play();
-    }
-
-    leaveAgain() {
-        document.title = LEAVE_TITLE;
-        this.portrait.classList.add("visible");
-    }
-
-    initWithVisibilityAPI() {
+    /**
+     * Init listening to visibility change events. Play "come back" on hidden and unloaded events.
+     */
+    initVisibilityEvents() {
         document.addEventListener("visibilitychange", () => {
-            console.log(document.visibilityState);
-        
+
+            // Log the visibilitychange event
+            console.log(`${new Date().toLocaleTimeString()}: visibilitychange => ${document.visibilityState}`);
+
             switch (document.visibilityState) {
             case "hidden":
             case "unloaded":
-                this.comeBack();
+                // Update the "come back" title and play the "come back" audio
+                document.title = "come back to me";
+                this.comeBackAudio.play();
                 break;
             case "visible":
-                this.leaveAgain();
+                // Update the "leave" title and make sure the "portrait" is visible
+                document.title = "leave page";
+                this.portrait.classList.add("visible");
                 break;
             }
         });
     }
 
-    initWithBlur() {
-        window.addEventListener("blur", () => {
-            this.comeBack();
-        });
-        
-        window.addEventListener("focus", () => {
-            this.leaveAgain();
-        });
-    }
-
+    /**
+     * Init the application.
+     */
     init() {
-
-
         document.getElementById("soundOnButton").addEventListener("click", () => {
+            // Toggle "panels"
             document.getElementById("confirmationPanel").classList.add("hidden");
             document.getElementById("instructionsPanel").classList.add("visible");
-            this.invisibleAudio.load();
 
-            switch (location.search) {
-            case "?b":
-                this.initWithBlur();
-                break;
-            case "?vb":
-                this.initWithVisibilityAPI();
-                this.initWithBlur();
-                break;
-            case "?v":
-            default:
-                this.initWithVisibilityAPI();
-                break;
-            }
+            // Mobile browsers have a setting for requiring a "user gesture" to play or pause an audio or video element
+            // See https://blog.foolip.org/2014/02/10/media-playback-restrictions-in-blink/
+            // That's why we need the "sound on" button: to load the "come back" audio element when the user clicks the button
+            this.comeBackAudio.load();
+
+            // Start listening to visibility change events
+            this.initVisibilityEvents();
+
+            // Show yet another "leave page" message
+            console.log("%cleave page already", "font-size: 2rem; font-weight: bold;");
         });
     }
 }
 
+/**
+ * Init the application on window load event.
+ */
 window.addEventListener("load", () => {
     const app = new App();
     app.init();
