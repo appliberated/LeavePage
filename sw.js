2bd916a69f3bcc22894cd7b379b319111e1055e1
Appliberated.com <develop@appliberated.com>
2017-10-27 14:09:43 +0300

    Improve service worker, delete old caches

--- sw.js
+++ sw.js
@@ -1,30 +1,48 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("leavepage").then(cache => {
-            return cache.addAll([
-                "/",
-                "/?b",
-                "/?vb",
-                "/?v",
-                "/index.html",
-                "/css/main.css",
-                "/js/main.js",
-                "/assets/images/somewhere-in-time.jpg",
-                "/assets/sounds/come-back-to-me.wav",
-                "/assets/images/cursors/cursor-leave.svg"
-            ])
-                .then(() => self.skipWaiting());
+/**
+ * leave page
+ * https://leavepage.appliberated.com/
+ *
+ * Copyright (c) 2017 Appliberated
+ * Released under the MIT license
+ * https://github.com/appliberated/LeavePage/blob/master/LICENSE
+ */
+
+"use strict";
+
+var CACHE_NAME = "leavepage-cache-v1";
+const URLS_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/css/main.css",
+    "/js/main.js",
+    "/assets/images/somewhere-in-time.jpg",
+    "/assets/sounds/come-back-to-me.wav",
+    "/assets/images/cursors/cursor-leave.svg"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(URLS_TO_CACHE).then(() => self.skipWaiting());
         })
     );
 });
 
+function deleteCaches (filter) {
+    return caches.keys()
+      .then(keys => filter ? keys.filter(filter) : keys)
+      .then(keys => keys.map(key => caches.delete(key)))
+      .then(deletions => Promise.all(deletions));
+}
+
 self.addEventListener("activate", event => {
-    event.waitUntil(self.clients.claim());
+    deleteCaches(name => name !== CACHE_NAME);
+    return event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request, { ignoreSearch: true }).then(response => {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
     );
